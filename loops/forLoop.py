148a5bf96308b4a1aabf6bbe27604bfc1212fde2
keitharwil <keith110606@gmail.com>
2025-08-30 01:57:57 +0800

    complete forLoop

--- loops/forLoop.py
+++ loops/forLoop.py
@@ -1,18 +1,28 @@
 import sys  
 #pip install terminaltables3
-from terminaltables3 import AsciiTable
+from terminaltables3 import DoubleTable
 
 def main():
+    data = []
     try:
         num = int(input("Enter number of Multiplication Table: "))
-        for i in range(int(input("Multiplication Table up to what number: "))):
-            print(multiply(num, i))
+        multiplied = int(input("Multiplication Table up to what number: "))
+        data.append([f"Multiplication table for {num}"])
+
+        for row in range(multiplied + 1):
+            inner_list = []
+            inner_list.append(multiply(num, row))
+            data.append(inner_list)
+        
+        table = DoubleTable(data)
+        table.inner_row_border = True
+        table.justify_columns[0] = 'center'
+        print(table.table)
     except ValueError:
         sys.exit("Input integer only")
 
 def multiply(num, i):
     return f"{num} x {i} = {num * i}"
-
 
 if __name__ == "__main__":
     main()
