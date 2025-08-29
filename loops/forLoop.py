3a657693c8bf781dd12de70ad2bccd01c1fbe0e9
keitharwil <keith110606@gmail.com>
2025-08-30 00:02:28 +0800

    Add new assignment Made forLoop not completed

---
+++ loops/forLoop.py
@@ -0,0 +1,16 @@
+import sys  
+
+def main():
+    try:
+        num = int(input("Enter number of Multiplication Table: "))
+        for i in range(int(input("Multiplication Table up to what number: "))):
+            print(multiply(num, i))
+    except ValueError:
+        sys.exit("Input integer only")
+
+def multiply(num, i):
+    return f"{num} x {i} = {num * i}"
+
+
+if __name__ == "__main__":
+    main()
