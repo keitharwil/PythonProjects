b99406339189014aa87ef3df25d6acf2200ec492
keitharwil <keith110606@gmail.com>
2025-08-30 13:00:09 +0800

    Complete whileLoop

---
+++ loops/whileLoop.py
@@ -0,0 +1,19 @@
+import sys
+from terminaltables3 import SingleTable
+
+def main():
+    data = []
+    data.append(["Even Numbers up to 100"])
+    i = 0
+    while i <= 100:
+        if i % 2 ==  0:
+            row_list = []
+            row_list.append(i)
+            data.append(row_list)
+        i += 1
+        
+    table = SingleTable(data)
+    print(table.table)
+        
+if __name__ == "__main__":
+    main()
