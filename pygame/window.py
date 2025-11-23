375b0c6b5ec027dd5270c286cbcf20b69471b014
keithkat <keith110606@gmail.com>
2025-11-23 21:51:33 +0800

    STARTINGS

--- pygame/window.py
+++ pygame/window.py
@@ -2,14 +2,21 @@ import pygame
 import sys
 
 pygame.init()
-screen = pygame.display.set_mode((1600,900))
+screen = pygame.display.set_mode((800,400)) #Creates window called 'screen' then set_mode' the size of the 'screen'
 pygame.display.set_caption("WINDOW")
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() # Clock object to control FPS for the game
+
+
+test_surface = pygame.image.load()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    screen.blit(test_surface,(200,100))
+
     pygame.display.update()
+    #Tells while True loop to not go above 60 fps
     clock.tick(60)
