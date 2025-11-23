c0d6432b26e5f34805ed69c18cf69def96603236
keithkat <keith110606@gmail.com>
2025-11-23 21:18:13 +0800

    Code for creating a simple window

--- pygame/window.py
+++ pygame/window.py
@@ -1,7 +1,12 @@
 import pygame
+import sys
 
 pygame.init()
-
 screen = pygame.display.set_mode((800,400))
 
 while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
